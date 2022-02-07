f1bbaa364c5aea13b0b2829b65ff4fbd43833468
oldh <cq0118@126.com>
2022-02-07 19:42:09 +0800

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,164 @@
+﻿/*
+ * Copyright (c) 2018 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
+ * distribute, sublicense, create a derivative work, and/or sell copies of the 
+ * Software in any work that is designed, intended, or marketed for pedagogical or 
+ * instructional purposes related to programming, coding, application development, 
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works, 
+ * or sale is expressly withheld.
+ *    
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public Board board;
+
+    public GameObject whiteWorker;
+
+    public GameObject blackWorker;
+
+    public GameObject purpleWorker;
+
+    private GameObject[,] pieces;
+
+    private Player white;
+    private Player black;
+    private Player purple;
+    public Player currentPlayer;
+    public Player otherPlayer;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start ()
+    {
+        pieces = new GameObject[8, 8];
+
+        white = new Player("white", true);
+        black = new Player("black", false);
+        purple = new Player("purple", false);
+        currentPlayer = white;
+        otherPlayer = black;
+
+        InitialSetup();
+    }
+
+    private void InitialSetup()
+    {
+        AddPiece(whiteWorker, white, 0, 1);
+        AddPiece(whiteWorker, white, 1, 0);
+
+        AddPiece(blackWorker, black, 0, 6);
+        AddPiece(blackWorker, black, 1, 7);
+
+        AddPiece(purpleWorker, purple, 6, 0);
+        AddPiece(purpleWorker, purple, 7, 1);
+    
+    }
+
+    public void AddPiece(GameObject prefab, Player player, int col, int row)
+    {
+        GameObject pieceObject = board.AddPiece(prefab, col, row);
+        player.pieces.Add(pieceObject);
+        pieces[col, row] = pieceObject;
+    }
+
+    public void SelectPieceAtGrid(Vector2Int gridPoint)
+    {
+        GameObject selectedPiece = pieces[gridPoint.x, gridPoint.y];
+        if (selectedPiece)
+        {
+            board.SelectPiece(selectedPiece);
+        }
+    }
+
+    public void SelectPiece(GameObject piece)
+    {
+        board.SelectPiece(piece);
+    }
+
+    public void DeselectPiece(GameObject piece)
+    {
+        board.DeselectPiece(piece);
+    }
+
+    public GameObject PieceAtGrid(Vector2Int gridPoint)
+    {
+        if (gridPoint.x > 7 || gridPoint.y > 7 || gridPoint.x < 0 || gridPoint.y < 0)
+        {
+            return null;
+        }
+        return pieces[gridPoint.x, gridPoint.y];
+    }
+
+    public Vector2Int GridForPiece(GameObject piece)
+    {
+        for (int i = 0; i < 8; i++) 
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                if (pieces[i, j] == piece)
+                {
+                    return new Vector2Int(i, j);
+                }
+            }
+        }
+
+        return new Vector2Int(-1, -1);
+    }
+
+    public bool FriendlyPieceAt(Vector2Int gridPoint)
+    {
+        GameObject piece = PieceAtGrid(gridPoint);
+
+        if (piece == null) {
+            return false;
+        }
+
+        if (otherPlayer.pieces.Contains(piece))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public bool DoesPieceBelongToCurrentPlayer(GameObject piece)
+    {
+        return currentPlayer.pieces.Contains(piece);
+    }
+
+    public void Move(GameObject piece, Vector2Int gridPoint)
+    {
+        Vector2Int startGridPoint = GridForPiece(piece);
+        pieces[startGridPoint.x, startGridPoint.y] = null;
+        pieces[gridPoint.x, gridPoint.y] = piece;
+        board.MovePiece(piece, gridPoint);
+    }
+}
